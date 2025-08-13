b6defb53b41a4110fdda537dfd0ddd9ae5c8cd3f
MFaisal077 <mohammadfaisal87897@gmail.com>
2025-08-13 21:40:38 +0100

    My first commit

---
+++ src/main/java/dev/faisal/Movie/Reservation/Repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import dev.faisal.;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface MovieRepository extends CrudRepository<Movie, Integer> {
+
+}
---
+++ src/main/java/dev/faisal/Movie/Reservation/model/Movies.java
@@ -0,0 +1,79 @@
+package model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movies {
+
+
+
+    private String Name;
+    private String Genre;
+    private String Year;
+    private String Director;
+    private String Rating;
+    @Id
+    @GeneratedValue()
+    private Integer id;
+
+
+    public Movies(String Name, String Genre, String Year, String Director, String Rating) {
+        this.Name = Name;
+        this.Genre = Genre;
+        this.Year = Year;
+        this.Director = Director;
+    }
+
+
+    public Movies() {}
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public void setGenre(String genre) {
+        Genre = genre;
+    }
+
+    public String getYear() {
+        return Year;
+    }
+
+    public void setYear(String year) {
+        Year = year;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public void setDirector(String director) {
+        Director = director;
+    }
+
+    public String getRating() {
+        return Rating;
+    }
+
+    public void setRating(String rating) {
+        Rating = rating;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
